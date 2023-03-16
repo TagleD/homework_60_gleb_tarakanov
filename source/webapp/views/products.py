d6bce4be0e35dc8ba9120dfc9aaa824ac44015f5
GLeb Tarakanov <glebtgd@gmail.com>
2023-03-16 20:31:55 +0600

    Реализована возможность добавления товара в коризну на странице детального просмотра

--- source/webapp/views/products.py
+++ source/webapp/views/products.py
@@ -1,12 +1,23 @@
 from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 from webapp.models import Product
-from webapp.forms import ProductForm
+from webapp.forms import ProductForm, BasketAddForm
 
 
 class ProductDetailView(DetailView):
     template_name = 'products/product_detail.html'
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['basket_add_form'] = BasketAddForm()
+        context.update(kwargs)
+        return super().get_context_data(**context)
+
+    # def get(self, request, *args, **kwargs):
+    #     self.object = self.get_object()
+    #     context = self.get_context_data(object=self.object)
+    #     return self.render_to_response(context)
 
 
 class ProductCreateView(CreateView):
